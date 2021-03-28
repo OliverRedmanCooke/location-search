3438f4cd9a62ee5dd53901a1e2d67ceb4e84f039
OliverRedmanCooke <oliver.r.cooke@gmail.com>
2021-03-28 18:20:40 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,33 @@ import { getList } from "./services/locations";
 import "./App.css";
 
 function App() {
-
   const [alert, setAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [loading, setloading] = useState(false);
   const [list, setList] = useState([]);
-  const [itemInput, setItemInput] = useState('');
+  const [itemInput, setItemInput] = useState("");
 
   const handleSubmit = (e) => {
-
-    setAlert(false)
-
     e.preventDefault();
-    getList(itemInput).then((res) => {
-        setList(res.data);
-    }).catch(err =>{
-      setList([])
-      setAlert(true)
-      console.log(err)
-    })
+    if (itemInput.length > 2) {
+      // reset these values on submit
+      setloading(true);
+      setAlert(false);
+      getList(itemInput)
+        .then((res) => {
+          setList(res.data);
+          setloading(false);
+        })
+        .catch((err) => {
+          setList([]);
+          setAlert(true);
+          setloading(false);
+          console.log(err);
+        });
+    } else {
+      setAlert(true);
+      setAlertMessage("You need to enter more than two characters to use this search function")
+    }
   };
 
   return (
@@ -28,17 +38,22 @@ function App() {
       <header className="App-header">
         <h1>Search API</h1>
       </header>
-      <form onSubmit={handleSubmit} >
+      <form onSubmit={handleSubmit}>
         <label>
-          <input onChange={event => setItemInput(event.target.value)} value={itemInput} type="text" placeholder="Search for a location" />
+          <input
+            onChange={(event) => setItemInput(event.target.value)}
+            value={itemInput}
+            type="text"
+            placeholder="Search for a location"
+          />
         </label>
         <button type="submit">Submit</button>
       </form>
       <ul className="location-list">
-        {list.map((item) => (
-          <li key={item.geonameid} >{item.name}</li>
-        ))}
-         {alert && <h2> There was an error getting these results</h2>}
+        {loading && <h2> Getting results....</h2>}
+        {!loading &&
+          list.map((item) => <li key={item.geonameid}>{item.name}</li>)}
+        {alert && <h2> {alertMessage}</h2>}
       </ul>
     </div>
   );
